cfbe7865bd7f1ecc50015df920ae456331c0322f
ricdferrer <ric.ferrer@outlook.com>
2020-06-29 12:51:55 -0300

    added web test feature

--- src/test/java/br/ce/cucumberferrer/steps/InserirContasSteps.java
+++ src/test/java/br/ce/cucumberferrer/steps/InserirContasSteps.java
@@ -3,77 +3,67 @@ package br.ce.cucumberferrer.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InserirContasSteps {
+
+    private final String DRIVER_PATH = "C:\\WebDrivers\\chromedriver_win32\\83\\chromedriver.exe";
+    public WebDriver driver;
+
     @Given("que estou acessando a aplicação")
     public void queEstouAcessandoAplicacao() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+        driver = new ChromeDriver();
+        driver.get("https://srbarriga.herokuapp.com/login");
     }
 
     @When("informo o usuário {string}")
     public void informoUsuario(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.id("email")).sendKeys(string);
     }
 
     @When("a senha {string}")
     public void aSenha(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.id("senha")).sendKeys(string);
     }
 
     @When("seleciono entrar")
     public void selecionoEntrar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.tagName("button")).click();
     }
 
     @Then("visualizo a página inicial")
     public void visualizoAPaginaInicial() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+        Assert.assertEquals("Bem vindo, wagner!", texto);
     }
 
     @When("seleciono Contas")
     public void selecionoContas() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();
     }
 
     @When("seleciono Adicionar")
     public void selecionoAdicionar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.xpath("//a[contains(text(),'Adicionar')]")).click();
     }
 
     @When("informo a conta {string}")
     public void informoAConta(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.xpath("//input[@id='nome']")).sendKeys(string);
     }
 
     @When("seleciono Salvar")
     public void selecionoSalvar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
     }
 
     @Then("a conta é inserida com sucesso")
     public void aContaInseridaComSucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("sou notificar que o nome da conta é obrigatório")
-    public void souNotificarQueONomeDaContaObrigatorio() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("sou notificado que já existe uma conta com esse nome")
-    public void souNotificadoQueJaExisteUmaContaComEsseNome() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+        Assert.assertEquals("Conta adicionada com sucesso!", texto);
     }
 }
--- src/test/resources/features/inserir_conta.feature
+++ src/test/resources/features/inserir_conta.feature
@@ -2,7 +2,7 @@ Feature: Cadastro de contas
 
   Como um usuário
   Gostaria de cadastrar contas
-  Para que eu possa distribuir meu dinheiro de uma forma mais organizada
+  Para que eu possa acessar minha aplicação
 
   Scenario: Deve inserir uma conta com sucesso
     Given que estou acessando a aplicação
@@ -12,29 +12,6 @@ Feature: Cadastro de contas
     Then visualizo a página inicial
     When seleciono Contas
     And seleciono Adicionar
-    And informo a conta "Conta de Teste"
+    And informo a conta "Conta de Teste Automação"
     And seleciono Salvar
     Then a conta é inserida com sucesso
-
-  Scenario: Não deve inserir uma conta sem nome
-    Given que estou acessando a aplicação
-    When informo o usuário "a@a"
-    And a senha "a"
-    And seleciono entrar
-    Then visualizo a página inicial
-    When seleciono Contas
-    And seleciono Adicionar
-    And seleciono Salvar
-    Then sou notificar que o nome da conta é obrigatório
-
-  Scenario: Não deve inserir uma conta com nome já existente
-    Given que estou acessando a aplicação
-    When informo o usuário "a@a"
-    And a senha "a"
-    And seleciono entrar
-    Then visualizo a página inicial
-    When seleciono Contas
-    And seleciono Adicionar
-    And informo a conta "Conta de Teste"
-    And seleciono Salvar
-    Then sou notificado que já existe uma conta com esse nome
